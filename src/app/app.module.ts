de83d206049f94cfd8244f430d5684bed966b2b4
Mike Wunderlich <ff@undefined-solutions.de>
2021-10-05 20:48:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagedjsComponent } from './pagedjs/pagedjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagedjsComponent
   ],
   imports: [
     BrowserModule
